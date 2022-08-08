317ec90ae35876234201a8e3617e004a6722d1a8
Joe Madden <joe@nayya.com>
2022-08-08 11:25:04 -0400

    feat(input): update to input props

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useField } from 'formik';
 import { FormGroup } from './style';
 
-export interface InputProps {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string;
   name: string;
   label: string;
   placeholder?: string;
-  value?: string;
   className?: string;
   variant?: 'contained' | 'normal';
 }
@@ -19,20 +19,33 @@ const Input = ({
   className,
   name,
   variant,
+  ...rest
 }: InputProps): JSX.Element => {
   const [field, meta] = useField(name);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleLabelClick = (): void => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <FormGroup
-      className={`formGroup ${className || ''} ${variant || 'normal'}`}
+      className={`formGroup ${className || ''} ${variant || 'normal'} ${
+        meta.value ? 'populated' : ''
+      }`}
     >
       <input
         id={id}
         type="text"
         placeholder={placeholder}
         {...field}
+        {...rest}
         className="inputField"
+        ref={inputRef}
       />
-      <label htmlFor={id} className="inputLabel">
+      <label htmlFor={id} className="inputLabel" onClick={handleLabelClick}>
         {label}
       </label>
       {meta.touched && meta.error && <div className="helper">{meta.error}</div>}
--- src/Input/style.ts
+++ src/Input/style.ts
@@ -11,25 +11,59 @@ export const FormGroup = styled.div`
   &.contained {
     position: relative;
 
+    input {
+      padding: 0.75rem;
+      font-size: 1rem;
+      line-height: 2.125rem;
+      color: var(--palette-isabelline-9);
+      border: 1px solid var(--palette-isabelline-2);
+      border-radius: 3px;
+      height: 60px;
+      box-sizing: border-box;
+    }
+
+    input:focus {
+      border: 2px solid var(--palette-cobalt-5);
+    }
+
     .inputLabel {
       position: absolute;
       display: block;
+      color: var(--palette-isabelline-6);
     }
 
+    /* Hides the placeholder as we don't use it in the Nayya componnent */
     .inputField::placeholder {
       color: transparent;
     }
 
+    /* input is not focused, and has no value */
+    /* centers the label and font size is set a bit larger */
     .inputField:placeholder-shown ~ .inputLabel {
       cursor: text;
-      top: 0;
+      top: 12px;
+      left: 12px;
       font-size: 1rem;
+      line-height: 2.125rem;
     }
 
-    .inputField:focus ~ .inputLabel {
-      top: 0;
+    /* input is focused or the input has a value */
+    /* moves the label up and shrinks the font size */
+    .inputField:focus ~ .inputLabel,
+    &.populated .inputField ~ .inputLabel {
+      top: 12px;
+      left: 12px;
       transition: 0.2s;
       font-size: 0.75rem;
+      line-height: 1rem;
+    }
+
+    /* switch up the padding when the input is populated or focused */
+    /* this pushes down the input value further below the label */
+    &.populated input,
+    input:focus {
+      padding: 1.75rem 0.75rem 0.75rem;
+      line-height: 1rem;
     }
   }
 `;
