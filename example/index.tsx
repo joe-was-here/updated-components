e9c98d2e4a354f0d6383a47ab979be8a22340e05
Joe Madden <joe@nayya.com>
2022-08-10 17:42:24 -0400

    refactor(git): remove main test workflow

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Input } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Input id="testing" name="testing" label="Here we go" />
     </div>
   );
 };
